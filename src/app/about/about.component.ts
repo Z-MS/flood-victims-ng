37bc05dd4346adc4a336d0d3da21d391ccef405a
Zayyad Muhammad Sani <zayyadmuhammed700@gmail.com>
2026-02-02 08:38:18 +0100

    Added firebase to the project

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-about',
   imports: [],
-  template: '',
-  styles: ''
+  template:  `
+    <div>
+        <section id="about-section">
+            <h2 className='section-title'>About the Project</h2>
+            <p>This is a data collection project for the 2024 Maiduguri flood victims. The project is sponsored by Swift Relief Foundation.</p>
+        </section>
+    </div>
+  `,
+  styles:  `
+    #contact, #about-section {
+      text-align: left;
+    }
+  `
 })
 export class AboutComponent {
 
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,22 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() => 
+      initializeApp({ projectId: "flood-victims", appId: "1:1009735267550:web:104e080090c7a5bcc77e4f",
+        storageBucket: "flood-victims.firebasestorage.app",
+        apiKey: "AIzaSyA4QOqDoZiGCKvcrabeuL__xwav1lYj698",
+        authDomain: "flood-victims.firebaseapp.com",
+        messagingSenderId: "1009735267550"
+      })),
+      provideAuth(() => getAuth()), 
+      provideFirestore(() => getFirestore())
+  ]
 };
