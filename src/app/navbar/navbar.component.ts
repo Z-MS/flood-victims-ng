8020957ca766af19451b63cb78b2c8ae237a70f6
Zayyad Muhammad Sani <zayyadmuhammed700@gmail.com>
2026-02-09 15:37:09 +0100

    Added hamburger toggle function

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, ElementRef, inject, ViewChild } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../auth/auth.service';
 import { AsyncPipe } from '@angular/common';
@@ -9,7 +9,7 @@ import { AsyncPipe } from '@angular/common';
 	styleUrl: './navbar.component.css',
 	template: `
         <header>
-            <button (onclick)="toggleNavbar()" class="nav-toggle" aria-label="open navigation" aria-controls="nav-list" aria-expanded="false">
+            <button #navToggle (click)="toggleNavbar()" class="nav-toggle" aria-label="open navigation" aria-controls="nav-list" aria-expanded="false">
                 <span class="hamburger">
                     <svg height="20" width="20">
                         <path d="M 2 4 L 18 4 M 2 10 L 18 10 M 2 16 L 18 16" stroke="white" stroke-width="2"/>
@@ -25,7 +25,7 @@ import { AsyncPipe } from '@angular/common';
                 <a id="logo" routerLink="/"><img src="logo.jpg" width="30" height="30" alt='Swift relief foundation logo'/></a>
             </div>
             <nav id='header-nav'>
-            <ul data-visible="false" class='nav-list'>
+            <ul #sideNavbar data-visible="false" class='nav-list'>
               <li class='nav-item'><a class='nav-link' routerLink="/">Home</a></li>
               <li class='nav-item'><a class='nav-link' routerLink="/displaced">Displaced Persons</a></li>
               <li id='about' class='nav-item'><a class='nav-link' routerLink="/about">About</a></li>
@@ -50,6 +50,8 @@ import { AsyncPipe } from '@angular/common';
 export class Navbar {
 	authService = inject(AuthService);
 	router = inject(Router);
+    @ViewChild('navToggle') navToggleRef!: ElementRef<HTMLButtonElement>;
+    @ViewChild('sideNavbar') sideNavbarRef!: ElementRef<HTMLUListElement>;
 
 	ngOnInit(): void {
 		// check if user is logged in
@@ -61,8 +63,15 @@ export class Navbar {
 	}
 
 	toggleNavbar() {
-
-	}
+        const visibility = this.sideNavbarRef?.nativeElement.getAttribute('data-visible');
+        if(visibility === 'false') {
+            this.sideNavbarRef?.nativeElement.setAttribute('data-visible', 'true')
+            this.navToggleRef?.nativeElement.setAttribute('aria-expanded', 'true')
+        } else {
+            this.sideNavbarRef?.nativeElement.setAttribute('data-visible', 'false')
+            this.navToggleRef?.nativeElement.setAttribute('aria-expanded', 'false')
+        }
+    }
 
 	signOutUser() {
 		this.authService.logout();
